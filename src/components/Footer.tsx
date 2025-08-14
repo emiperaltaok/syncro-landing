6e13bf60b497786244ea937474fe4960e778a265
emiperaltaok <emiperaltaok@gmail.com>
2025-08-14 14:57:05 -0300

    detectando error v1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -98,7 +98,7 @@ export function Footer() {
         <div className="my-10 text-sm text-center text-gray-600 dark:text-gray-400">
           Copyright © {new Date().getFullYear()}. Made with ♥ by{" "}
           <a href="https://web3templates.com/" target="_blank" rel="noopener">
-            Web3TemplatesEmiPeralta.
+            Web3TemplatesEmiPeraltaok
           </a>{" "}
           Illustrations from{" "}
           <a href="https://www.glazestock.com/" target="_blank" rel="noopener ">
