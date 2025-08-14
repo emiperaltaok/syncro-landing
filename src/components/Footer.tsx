985502f7e081bfd6c0ffebdf4d0ba3b493c386c8
emiperaltaok <emiperaltaok@gmail.com>
2025-08-14 16:35:25 -0300

    agregue syncro logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ export function Footer() {
                 className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100"
               >
                 <Image
-                  src="/img/logo.svg"
+                  src="/img/syncro-logo.png"
                   alt="N"
                   width="32"
                   height="32"
