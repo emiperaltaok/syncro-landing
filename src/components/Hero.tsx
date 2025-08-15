0fb7aa3b62e0f24373b075567a520ccd5fdd106e
emiperaltaok <emiperaltaok@gmail.com>
2025-08-15 14:46:20 -0300

    titulo stack tecnologico

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -59,8 +59,7 @@ export const Hero = () => {
       <Container>
         <div className="flex flex-col justify-center">
           <div className="text-xl text-center text-gray-700 dark:text-white">
-            Trusted by <span className="text-indigo-600">2000+</span>{" "}
-            customers worldwide
+            Tecnologias que potencian nuestras soluciones
           </div>
 
           <div className="flex flex-wrap justify-center gap-5 mt-10 md:justify-around">
