0b03d7b185a20994cdc199107dd83929688c2569
emiperaltaok <emiperaltaok@gmail.com>
2025-08-19 20:31:58 -0300

    cta a mi whatsapp

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -15,7 +15,7 @@ export const Cta = () => {
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
           <a
-            href="#contacto"
+            href="https://wa.me/5493512332525"
             className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 hover:bg-gray-100"
           >
             Consulta Gratuita
