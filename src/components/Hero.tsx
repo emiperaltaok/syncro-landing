210ce0475f0aa27f30ae035ea7aa2fc7cd3751a0
emiperaltaok <emiperaltaok@gmail.com>
2026-05-16 15:09:50 -0300

    design: hero fix syntax-fix: resolve hero merge conflict

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,7 +34,7 @@ export const Hero = () => {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               
-                href="https://wa.me/5493512332525"
+                <a href="https://wa.me/5493512332525"
                 target="_blank"
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
