0c9eb88ff112b2d775354d70332f4733d14c61e8
emiperaltaok <emiperaltaok@gmail.com>
2026-05-16 15:22:46 -0300

    design: hero button off-white

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -37,7 +37,7 @@ export const Hero = () => {
                 <a href="https://wa.me/5493512332525"
                 target="_blank"
                 rel="noopener"
-                className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
+                className="px-8 py-4 text-lg font-medium text-center text-[#0d0d0d] bg-[#f0ede6] rounded-md ">
                 Consulta Gratuita
               </a>
             </div>
